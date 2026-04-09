6e21469724246a74455fcffc3a1387387c4492f4
mahinder <mahinder124@gmail.com>
2026-04-09 19:42:05 +0530

    fix(api): explicitly map businessUnitCode to id in REST response for intuitive testing

--- src/main/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseResourceImpl.java
+++ src/main/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseResourceImpl.java
@@ -127,6 +127,7 @@ public class WarehouseResourceImpl implements WarehouseResource {
   private Warehouse toWarehouseResponse(
       com.fulfilment.application.monolith.warehouses.domain.models.Warehouse warehouse) {
     var response = new Warehouse();
+    response.setId(warehouse.businessUnitCode);
     response.setBusinessUnitCode(warehouse.businessUnitCode);
     response.setLocation(warehouse.location);
     response.setCapacity(warehouse.capacity);
